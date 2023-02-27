4ea65b241f8d37e61efc54bd74af5726a8cd6a22
TaeHyeon <118148291+taehyeon83@users.noreply.github.com>
2023-02-27 19:30:23 +0900

    Add files via upload

---
+++ for.js
@@ -0,0 +1,3 @@
+for (let i = 0; i < str.length; i++) {
+  //문자열 전체를 보는 방법
+}
---
+++ string.js
@@ -0,0 +1,8 @@
+alert("hellow\nworld");
+// \n 줄바꿈
+alert("hello" + " " + "world");
+// "공백" 띄어쓰기
+alert("hellow world".length);
+// .length = 문자길이
+alert("hellow world".indexOf("o"));
+// .indexOf = 문자열 내 특정한 문자열의 index값을 리턴
