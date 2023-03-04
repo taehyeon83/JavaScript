2e364d3b4fa4b57b31f639b53819ac8c5fb25a1e
TaeHyeon <118148291+taehyeon83@users.noreply.github.com>
2023-03-04 21:28:50 +0900

    Add files via upload

---
+++ variable.js
@@ -0,0 +1,25 @@
+// 1. Use strict
+// added in ES 5
+// use this for Valina Javascript
+"use styict";
+
+// 2. Variable
+// let (added in ES6) (mutable 데이터 타입)
+
+// var (don't use!)
+// var hoisting (어디든 상관없이 맨위로 끌어 올림)
+// var는 블록스코프가 없다.
+
+// 3. constans
+// 한번 설정한 값은 변경 불가 (immutable 데이터 타입)
+// -보안상 이유
+
+// 4. Variable type
+// js data tpes for 'number'
+// 정수든 소수점 수 상관없이 type은 number로 나타남.
+
+// boolean
+// false : 0, null, undefined, NaN, ''
+// true :  그 외 모든 값
+
+// Dynamic typing
